d821e57a3313b7c0c379197542ab74cbd254c082
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-11-26 20:01:52 +0630

    fix: add http 422

--- src/lib/HttpErrors.ts
+++ src/lib/HttpErrors.ts
@@ -1,8 +1,14 @@
 import { HttpApiSchema } from "@effect/platform";
 import { Schema as S } from "effect";
 
-export class BadRequest extends S.TaggedError<BadRequest>()(
-  "BadRequest",
+export class UnprocessableContent extends S.TaggedError<UnprocessableContent>()(
+  "UnprocessableContent",
   { message: S.String },
-  HttpApiSchema.annotations({ status: 400 }),
+  HttpApiSchema.annotations({ status: 422 }),
+) {}
+
+export class InternalServerError extends S.TaggedError<InternalServerError>()(
+  "InternalServerError",
+  { message: S.String },
+  HttpApiSchema.annotations({ status: 500 }),
 ) {}
