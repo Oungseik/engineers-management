399877953778dcb287988797cb7934e30c9dcb09
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-12-01 11:23:32 +0630

    feat: add api endpoint to add skills

--- src/Api/Skills/Api.ts
+++ src/Api/Skills/Api.ts
@@ -1,7 +1,7 @@
 import { HttpApiEndpoint, HttpApiGroup, OpenApi } from "@effect/platform";
 import { Schema as S } from "effect";
 
-import { InternalServerError } from "@/lib/HttpErrors";
+import { InternalServerError, UnprocessableContent } from "@/lib/HttpErrors";
 import { skillTags } from "@/schemas/sqlite";
 
 const Skill = S.Struct({
@@ -14,13 +14,26 @@ const getSkills = HttpApiEndpoint.get("get list of skills", "/")
   .addError(InternalServerError)
   .annotateContext(
     OpenApi.annotations({
-      title: "Skills",
+      title: "Get skills",
       description: "API endpoint to get list of skills which can select",
+    }),
+  );
+
+const postSkill = HttpApiEndpoint.post("add new skill in a skill list", "/")
+  .setPayload(S.Struct({ name: S.NonEmptyString, tag: S.Literal(...skillTags) }))
+  .addSuccess(S.Struct({ success: S.Literal(true) }))
+  .addError(UnprocessableContent)
+  .addError(InternalServerError)
+  .annotateContext(
+    OpenApi.annotations({
+      title: "Add Skill",
+      description: "API endpoint to add new skill in the skills list",
     }),
   );
 
 export const SkillsApi = HttpApiGroup.make("skills")
   .add(getSkills)
+  .add(postSkill)
   .prefix("/api/skills")
   .annotateContext(
     OpenApi.annotations({ title: "Skills APIS", description: "APIs to manage possible skills" }),
--- src/Api/Skills/Http.ts
+++ src/Api/Skills/Http.ts
@@ -1,5 +1,10 @@
 import { HttpApi, HttpApiBuilder } from "@effect/platform";
-import { Effect as Ef } from "effect";
+import { SqliteDrizzle } from "@effect/sql-drizzle/Sqlite";
+import { Effect as Ef, Effect } from "effect";
+
+import { handleSqlError } from "@/lib/ErrorHelpers";
+import { InternalServerError } from "@/lib/HttpErrors";
+import { skills } from "@/schemas/sqlite";
 
 import { SkillsApi } from "./Api";
 
@@ -7,8 +12,23 @@ const Api = HttpApi.empty.add(SkillsApi);
 
 export const SkillsApiLive = HttpApiBuilder.group(Api, "skills", (handlers) =>
   Ef.gen(function* () {
-    return handlers.handle("get list of skills", () =>
-      Ef.succeed([{ name: "TypeScript", tag: "programming language" }]),
-    );
+    const db = yield* SqliteDrizzle;
+
+    return handlers
+      .handle("get list of skills", () =>
+        db
+          .select()
+          .from(skills)
+          .pipe(
+            Effect.map((skills) => skills),
+            Effect.catchAll(() => new InternalServerError({ message: "something went wrong" })),
+          ),
+      )
+      .handle("add new skill in a skill list", ({ payload }) =>
+        db
+          .insert(skills)
+          .values({ ...payload, name: payload.name.toLowerCase() })
+          .pipe(Ef.andThen({ success: true } as const), Ef.catchTags({ SqlError: handleSqlError })),
+      );
   }),
 );
