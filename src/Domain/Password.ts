88a26e99cccb2e9b7207ffedd7525c8a82673121
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-12-07 10:30:58 +0630

    change the password regex

--- src/Domain/Password.ts
+++ src/Domain/Password.ts
@@ -1,11 +1,11 @@
 import { Schema as S } from "effect";
 
 export const Password = S.String.pipe(
-  S.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@.#$!%*?&]{8,15}$/),
+  S.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/),
   S.brand("Password"),
   S.annotations({
     title: "Password",
     description:
-      "A password which must contain at least one uppercase, one lowercase, and one number",
+      "A password which must contain at least one uppercase, one lowercase, one number, and no space.",
   }),
 );
